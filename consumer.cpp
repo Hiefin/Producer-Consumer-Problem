10045774da9eae7525265916fd62ece262ea62ef
Jacob Drahuschak Jr <151879582+Hiefin@users.noreply.github.com>
2025-10-26 21:55:08 -0400

    Update consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -12,6 +12,7 @@ int main() {
     int shmid = shmget(SHM_KEY, sizeof(shared_data), 0666);      // Shared Memory get using personalized key, with the size of Struct.
     shared_data* data = (shared_data*)shmat(shmid, nullptr, 0);  // Shared Memory attach so we can use shared struct like a normal struct
 
+    sleep(1);                                                    // Give time for producer to produce
     while (true) {
         sem_wait(&data->full);                  // Waits till there is a item in buffer
         sem_wait(&data->mutex);                 // Locks Mutex for mutual exlcusion
